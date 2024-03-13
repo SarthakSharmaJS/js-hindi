e20733172d7efb701ddd37964d204b98a6870bb4
SarthakSharmaJS <163293024+SarthakSharmaJS@users.noreply.github.com>
2024-03-13 16:27:42 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("Sarthak")
+
+
+let name = "Sarthak"
+let age = 16
+let isLoggedIn = false 
+let state;
+
+// number => 2 to power 53
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => uniqu
+
+
+// object 
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); //object
+
